81a8d626759a993f9ced24e17c411de75991aa23
bonganimasemola <bonganimasemola@icloud.com>
2023-11-05 12:41:31 +0300

    making edits pushed through - had a minor technical issue

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,27 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+  const navLinkStyle = {
+    margin: "10px",  // Add margin
+    padding: "10px", // Add padding
+    textDecoration: "none", // Remove underlines from links
+    color: "black" // Set text color to black
+  };
+
   return (
     <div>
-      <NavLink exact to="/">Home</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
-      <NavLink to="/directors">Directors</NavLink>
-      <NavLink to="/actors">Actors</NavLink>
+      <NavLink exact to="/" style={navLinkStyle}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" style={navLinkStyle}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={navLinkStyle}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" style={navLinkStyle}>
+        Actors
+      </NavLink>
     </div>
   );
 }
